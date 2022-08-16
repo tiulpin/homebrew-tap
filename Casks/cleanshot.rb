c0f78256e027aab5ad5652bad915855b6d007884
Viktor <viktor@tiulp.in>
2022-08-16 18:44:45 +0200

    Create cleanshot.rb

---
+++ Casks/cleanshot.rb
@@ -0,0 +1,29 @@
+cask "cleanshot" do
+  version "4.2.1"
+  sha256 "33465304f704c9bf264af65e3fb0e47643e2c3be0b8726e02760ed109f71553a"
+
+  url "https://updates.getcleanshot.com/v3/CleanShot-X-#{version}.dmg"
+  name "CleanShot"
+  desc "Screen capturing tool"
+  homepage "https://getcleanshot.com/"
+
+  livecheck do
+    url "https://cleanshot.com/changelog"
+    regex(/class="number">(\d+(?:\.\d+)+)/i)
+  end
+
+  auto_updates true
+  depends_on macos: ">= :sierra"
+
+  app "CleanShot X.app"
+
+  uninstall quit: "pl.maketheweb.cleanshotx"
+
+  zap trash: [
+    "~/Library/Application Support/CleanShot",
+    "~/Library/Caches/pl.maketheweb.cleanshotx",
+    "~/Library/Caches/SentryCrash/CleanShot X",
+    "~/Library/Preferences/com.getcleanshot.app.plist",
+    "~/Library/Preferences/pl.maketheweb.cleanshotx.plist",
+  ]
+end
